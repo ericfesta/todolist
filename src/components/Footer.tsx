a61c29b862e7cc4581eaedd0f1da1840f2f77770
Eric Festa <eric.festa@gmail.com>
2025-05-15 18:06:09 +0200

    Initial commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="mt-8 text-center text-sm text-gray-500 dark:text-gray-400 py-3 transition-colors">
+      Creato con ❤️ con ChatAndBuild
+    </footer>
+  );
+};
+
+export default Footer;
